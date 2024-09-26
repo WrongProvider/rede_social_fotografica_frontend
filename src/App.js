083a474af45c1a1cc541e4bca3feb2d78b6b369f
WrongProvider <guara.taste@gmail.com>
2024-09-25 21:42:28 -0300

    add Sidebar components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import styled from 'styled-components'
 import Header from './components/Header'
+import Sidebar from './components/Sidebar'
 
 function App() {
   return (
     <AppWrapper>
       <Header />
+      <div className="app__body">
+        <Sidebar />
+      </div>
       <h1>Rede social em foto MERN</h1>
     </AppWrapper>
   );
